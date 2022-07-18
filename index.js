09e4c4737087420069c243125a134c567b55fb64
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-07-17 22:06:16 -0300

    Configura sessions, pastas public, css, img, layout/main.handlebars

--- index.js
+++ index.js
@@ -8,6 +8,59 @@ const app = express()
 
 const conn = require('./db/conn')
 
+// template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// receber resposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// session middleware
+// resave - se cair a sessão, desconecta
+// maxAge: 360000 - 1 dia
+// httpOnly: true - sem certificado de segurança
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+   
+    if(req.session.userid) {
+       res.locals.session = req.session 
+    }
+
+    next()
+})
+
+// conexão banco
 conn
 .sync()
 .then(() => {
